c6bfd62f06da7276b3108f62c1ffc738e8c34d93
librizzia <librizzia@gmail.com>
2023-12-07 15:19:46 -0500

    day 5 solution (part 1)

---
+++ 2023/day5/solution.py
@@ -0,0 +1,15 @@
+from functools import reduce
+
+seeds, *mappings = open('input.txt').read().split('\n\n')
+seeds = map(int, seeds.split()[1:])
+
+def lookup(start, mapping):
+    for m in mapping.split('\n')[1:]:
+        dst, src, len = map(int, m.split())
+        delta = start - src
+        if delta in range(len):
+            return dst + delta
+    else: return start
+
+lowest_location = min(reduce(lookup, mappings, int(s)) for s in seeds)
+print(f"Part 1: {lowest_location}")
